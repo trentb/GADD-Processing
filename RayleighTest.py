ed8c66be6fdc10a42222f265e32252ed9ea537af
trentb <trent.borman@gmail.com>
2015-02-17 13:00:57 -0500

    Implemented Rayleigh into Diel
    Rayleigh behavior implemented into diel.py. Bootstrap included for
    confidence interval functionality.

--- RayleighTest.py
+++ RayleighTest.py
@@ -11,8 +11,11 @@ import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
 import os
+from scipy.stats.distributions import t
+import bootstrap as boot
 filename="7-31-14_An_XI-D1_100Hz-1MHz_2V.diel"
 
+#enter parameters for fitting
 thickness=1
 diameter=600
 minpts=10
@@ -20,6 +23,7 @@ maxpts=32
 maxi=8
 freq=1000
 
+#import data processing to get dielectric constant and electric field
 s,m=diel.getdata("7-31-14_An_XI-D1_100Hz-1MHz_2V.diel")
 E=[x*10/thickness for x in s["C AC_set"]]
 E=np.asarray(E)
@@ -27,13 +31,13 @@ data=m["CMPLX CD0"]
 data=[x.real*thickness*10**-6/((8.854*10**-12)*((diameter/2*10**-6)**2*np.pi))for x in data]
 k=np.array(data)
 k=k.reshape(len(s["A FREQUENCY_set"]),len(s["B COUNTER"]),len(s["C AC_set"]))
-
 freqindex=s["A FREQUENCY_set"].index(freq)
 #Placeholder variables:
 bestslopediff=1000
 bestslope=1
 bestrsq=0
 
+#fit over range of applicable data points to find best fit
 for i in range(0,maxi):
     for j in range(minpts,maxi+maxpts):
         if j-i < maxpts and j-i > minpts:
@@ -48,11 +52,12 @@ for i in range(0,maxi):
                 bestrsq=(r_value2**2+r_value3**2)/2
                 bestpvalue=(p_value2+p_value3)/2
                 beststderror=(std_err2+std_err3)/2
-
+#calculate average dielectric constant and bestfit line
 averagek=(k[freqindex,1,:]+k[freqindex,2,:])/2
 Efit=np.linspace(min(E),max(E),100)
 Kfit=bestintercept+bestslope*Efit
 
+#plot data
 plt.plot(E,averagek, 'o', linestyle='None', linewidth=0)
 plt.plot(Efit,Kfit)
 plt.ticklabel_format(style='plain')
@@ -61,7 +66,16 @@ plt.ylabel('Dielectric Constant', fontsize=18)
 plt.xlabel('Electric Field (kV/cm)', fontsize=18)
 plt.title('Rayleigh Data', fontsize=24)
 
-Ekdata=np.vstack((E,averagek))
+#calculate confidence intervals:
+cis=boot.ci((E[besti:bestj],averagek[besti:bestj]), statfunction=stats.linregress)
+
+#export data
+Ekdata=np.transpose(np.vstack((E,averagek)))
 savefile=os.path.splitext(filename)[0]
 savefile=savefile+"_average.csv"
-np.savetxt(savefile, Ekdata, delimiter=",",)
+f='The best i value is: ' + repr(besti) + '\n'
+f=f+'The best j value is: ' + repr(bestj) + '\n'
+f=f+'The best slope value is: ' + repr(round(bestslope,3)) + ' (' + repr(round(cis[0,0],3)) + ', ' + repr(round(cis[1,0],3)) + ')' + '\n'
+f=f+'The best intercept value is: ' + repr(round(bestintercept,3)) + ' (' + repr(round(cis[0,1],3)) + ', ' + repr(round(cis[1,1],3)) + ')' + '\n'
+f=f+'The best R^2 value is: ' + repr(round(bestrsq,3))
+np.savetxt(savefile, Ekdata, delimiter=", ", header=f, fmt='%.2f, %.2f')
--- diel.py
+++ diel.py
@@ -12,7 +12,7 @@ def getdata(filename):
     '''
     This function reads the .diel file and returns a dictionary with the values
     of each sweep as well as a dictionary with a list of each measurement.
-    
+
     Parameters
     ----------
     filename : (string)
@@ -70,8 +70,8 @@ def getdata(filename):
                     # real
                     else:
                         m[key].append(float(line))
-                        
-                        
+
+
             # Are we in the header?
             if '*' in line:
                 H = False
@@ -82,13 +82,13 @@ def createdf(data, rows, cols):
     Takes 1-D lists of data, rows, and cols to create a 2-D dataframe with the
     rows and columns labeled. The the rows, cols, and data should be in this
     order in the .diel file:
-    
+
         SWEEP (rows)
             SWEEP (cols)
                 MEAS (data)
             ENDSWEEP
         ENDSWEEP
-    
+
     Parameters
     ----------
     data : (list)
@@ -116,7 +116,7 @@ def createabsdf(data, rows, cols):
     Takes 1-D lists of data, rows, and cols to create a 2-D dataframe with the
     rows and columns labeled. This function returns the absolute value of the
     input data.
-    
+
     Parameters
     ----------
     data : (list)
@@ -143,9 +143,9 @@ def createabsdf(data, rows, cols):
 def zprime(eps, freq):
     '''
     This function takes a one dimensional list of complex permittivity data and
-    the corresponding list of frequencies and returns a one dimensional numpy 
+    the corresponding list of frequencies and returns a one dimensional numpy
     array of complex impedance.
-    
+
     Parameters
     ----------
     eps : (list)
@@ -166,13 +166,13 @@ def zprime(eps, freq):
     re = np.divide(eps.imag, denom)
     im = np.divide(-eps.real, denom)*1j
     return np.add(re, im)
-    
+
 def zprimeprime(eps, freq): # Depreciated
     '''
     This function takes a one dimensional list of complex permittivity data and
-    the corresponding list of frequencies and returns a one dimensional numpy 
+    the corresponding list of frequencies and returns a one dimensional numpy
     array of complex impedance.
-    
+
     Parameters
     ----------
     eps : (list)
@@ -192,13 +192,13 @@ def zprimeprime(eps, freq): # Depreciated
         im = -eps[i].real/(2*np.pi*freq[i]*(np.square(eps[i].imag)+np.square(eps[i].real)))
         result[i] = complex(re, im)
     return result
-    
+
 def impedance(filename):
     '''
     This function takes a .diel if impedance data and returns a dictionary of
     dataframes with columns of frequency, real impedance, and imaginary
     impedance for each temperature.
-    
+
     Parameters
     ----------
     filename : (string)
@@ -232,7 +232,7 @@ def ivloop(files):
     '''
     Reads .diel files and converts them to spreadsheets for plotting abs(I)-V
     curves.
-    
+
     Parameters
     ----------
     files : (list)
@@ -252,12 +252,12 @@ def ivloop(files):
         # Was the sample aged before the measurement?
         if 'REAL PA0' in m.keys():
             agefirst = True
-        
+
         if agefirst is True:
             df1 = createabsdf(m['REAL PA1'], s['B DC_set'], s['C TIME_set'])
             df2 = createabsdf(m['REAL PA3'], s['E DC_set'], s['F TIME_set'])
             df3 = createabsdf(m['REAL PA5'], s['H DC_set'], s['I TIME_set'])
-        
+
         elif agefirst is False:
             try:
                 df1 = createabsdf(m['REAL PA1'], s['A DC_set'], s['B TIME_set'])
@@ -266,26 +266,26 @@ def ivloop(files):
                 break
             df2 = createabsdf(m['REAL PA3'], s['D DC_set'], s['E TIME_set'])
             df3 = createabsdf(m['REAL PA5'], s['G DC_set'], s['H TIME_set'])
-            
+
         final = pd.concat([df1, df2, df3])
         xname = filename[:-5]+'_IVloop'
         f[xname] = final
     return f
-    
+
 def ageprogression(files):
     '''
     Pulls current during aging from .diel files
-    Reads .diel files and extracts the leakage current during aging. If 
-    multiple files are listed, aging data will be appended to a 
+    Reads .diel files and extracts the leakage current during aging. If
+    multiple files are listed, aging data will be appended to a
     pandas.DataFrame where the index is the time in minutes and the columns
     contain current values. The second column starts at the index immediately
     followng the previous column and so on.
-    
+
     Parameters
     ----------
     files : (list)
             List of .diel files to be imported (should be in order)
-            
+
     Returns
     -------
     DataFrame : (pandas.DataFrame)
@@ -301,52 +301,114 @@ def ageprogression(files):
             d[filename] = pd.Series(values, index=index)
             adj += len(values)
     return pd.DataFrame(d)
-    
+
+from scipy import stats
+import matplotlib.pyplot as plt
+import os
+from scipy.stats.distributions import t
+import bootstrap as boot
+
 def rayleigh(filename,freq,thickness,diameter,minpts,maxpts,maxi):
-        '''
-        Reads .diel file and extracts CD0/Voltage field for requested 
-        frequency for all 3 runs. Converts to dielectric constant and electric
-        field. Reshapes data and performs linear regressions on all ranges i:j
-        where i ranges from point 1 to max i. And j ranges from 1+minpts to
-        maxi maxpts. Averages linear regressions for counter 2 and 3 and checks
-        if average R^2 is better than existing average R^2. If this is the case,
-        A new best R^2 is saved along with the best i and j values, slope,
-        intercept and confidence intervals. Averages counter 2 and 3 data
-        and exports that data
-        
-        Paramters
-        ---------
-        filename : (string)
-                    Name of file of interest
-        freq : (integer)
-                    Frequency of interest in Hz
-        thickness : (float)
-                    Thickness of dielectric in microns
-        diameter : (float)
-                    Diameter of contact in microns
-        minpts : (integer)
-                    Minimum number of points for fitting
-        maxpts : (integer)
-                    Maximum number of points for fitting
-        maxi : (integer)
-                    Maximum value of i (start of fit)
-                    
-        Returns
-        -------
-        average : (numpy.array)
-                    Contains average dielectric constant and electric field data
-        besti : (integer)
-                    Best value for i by maximizing R^2
-        bestj : (integer)
-                    Best value for j by maximizing R^2
-        bestslope : (float)
-                    Best value of slope by maximizing R^2
-        bestint : (float)
-                    Best value of intercept by maximizing R^2
-        bestrsq : (float)
-                    Best value of R^2
-        bestconf : (list)
-                    95% confidence interval on slope and intercept
-        '''
-        
-        s,m = getdata(filename)
+    '''
+    Reads .diel file and extracts CD0/Voltage field for requested
+    frequency for all 3 runs. Converts to dielectric constant and electric
+    field. Reshapes data and performs linear regressions on all ranges i:j
+    where i ranges from point 1 to max i. And j ranges from 1+minpts to
+    maxi maxpts. Averages linear regressions for counter 2 and 3 and checks
+    if average R^2 is better than existing average R^2. If this is the case,
+    A new best R^2 is saved along with the best i and j values, slope,
+    intercept and confidence intervals. Averages counter 2 and 3 data
+    and exports that data
+
+    Paramters
+    ---------
+    filename : (string)
+                Name of file of interest
+    freq : (integer)
+                Frequency of interest in Hz
+    thickness : (float)
+                Thickness of dielectric in microns
+    diameter : (float)
+                Diameter of contact in microns
+    minpts : (integer)
+                Minimum number of points for fitting
+    maxpts : (integer)
+                Maximum number of points for fitting
+    maxi : (integer)
+                Maximum value of i (start of fit)
+
+    Returns
+    -------
+    average : (numpy.array)
+                Contains average dielectric constant and electric field data
+    besti : (integer)
+                Best value for i by maximizing R^2
+    bestj : (integer)
+                Best value for j by maximizing R^2
+    bestslope : (float)
+                Best value of slope by maximizing R^2
+    bestint : (float)
+                Best value of intercept by maximizing R^2
+    bestrsq : (float)
+                Best value of R^2
+    bestconf : (list)
+                95% confidence interval on slope and intercept
+    '''
+
+
+    #import data processing to get dielectric constant and electric field
+    s,m = getdata(filename)
+    E = [x*10/thickness for x in s["C AC_set"]]
+    E = np.asarray(E)
+    data = m["CMPLX CD0"]
+    data = [x.real*thickness*10**-6/((8.854*10**-12)*((diameter/2*10**-6)**2*np.pi))for x in data]
+    k = np.array(data)
+    k = k.reshape(len(s["A FREQUENCY_set"]),len(s["B COUNTER"]),len(s["C AC_set"]))
+    freqindex = s["A FREQUENCY_set"].index(freq)
+    #Placeholder variables:
+    bestslopediff = 1000
+    bestslope = 1
+    bestrsq = 0
+    #fit over range of applicable data points to find best fit
+    for i in range(0,maxi):
+        for j in range(minpts,maxi+maxpts):
+            if j-i < maxpts and j-i > minpts:
+                slope2, intercept2, r_value2, p_value2, std_err2 = stats.linregress(E[i:j],k[freqindex,1,i:j])
+                slope3, intercept3, r_value3, p_value3, std_err3 = stats.linregress(E[i:j],k[freqindex,2,i:j])
+                if bestrsq < (r_value2**2+r_value3**2)/2:
+                    bestslopediff = abs(slope2-slope3)
+                    besti = i
+                    bestj = j
+                    bestslope = (slope2+slope3)/2
+                    bestintercept = (intercept2+intercept3)/2
+                    bestrsq = (r_value2**2+r_value3**2)/2
+                    bestpvalue = (p_value2+p_value3)/2
+                    beststderror = (std_err2+std_err3)/2
+    #calculate average dielectric constant and bestfit line
+    averagek = (k[freqindex,1,:]+k[freqindex,2,:])/2
+    Efit = np.linspace(min(E),max(E),100)
+    Kfit = bestintercept+bestslope*Efit
+
+    #plot data
+    plt.plot(E,averagek, 'o', linestyle='None', linewidth=0)
+    plt.plot(Efit,Kfit)
+    plt.ticklabel_format(style='plain')
+    plt.tick_params(labelsize=14)
+    plt.ylabel('Dielectric Constant', fontsize=18)
+    plt.xlabel('Electric Field (kV/cm)', fontsize=18)
+    plt.title('Rayleigh Data', fontsize=24)
+    plt.axis('normal')
+
+    #calculate confidence intervals:
+    cis = boot.ci((E[besti:bestj],averagek[besti:bestj]), statfunction=stats.linregress)
+
+    #export data
+    Ekdata = np.transpose(np.vstack((E,averagek)))
+    savefile = os.path.splitext(filename)[0]
+    savefile = savefile+"_average.csv"
+    f = 'The best i value is: ' + repr(besti) + '\n'
+    f = f+'The best j value is: ' + repr(bestj) + '\n'
+    f =f+'The best slope value is: ' + repr(round(bestslope,3)) + ' (' + repr(round(cis[0,0],3)) + ', ' + repr(round(cis[1,0],3)) + ')' + '\n'
+    f = f+'The best intercept value is: ' + repr(round(bestintercept,3)) + ' (' + repr(round(cis[0,1],3)) + ', ' + repr(round(cis[1,1],3)) + ')' + '\n'
+    f = f+'The best R^2 value is: ' + repr(round(bestrsq,3))
+    np.savetxt(savefile, Ekdata, delimiter=", ", header=f, fmt='%.2f, %.2f')
